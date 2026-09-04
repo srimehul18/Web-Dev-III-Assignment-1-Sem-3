f8bbc759d58b89b7d3083e01a4ebd942016278cd
Mehul Srivastava <srimehul18@gmail.com>
2026-09-04 23:05:39 +0530

    Added console log to http server

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
+    console.log("Request received:", req.url)
     if (req.url === '/' && req.method === 'GET') {
-        res.end('<h1>Welcome to Assignment 1</h1>')
+        res.end('<h1>Welcome to Node Server</h1>')
     }
     else if (req.url === '/about' && req.method === 'GET') {
-        res.end('<h1>About Page</h1> <p>All about infro can be found here</p>')
+        res.end('<h1>About Page</h1> <p>All about info can be found here</p>')
     }
     else if (req.url === '/contact' && req.method === 'GET') {
-        res.end(JSON.stringify({
-            "Name": "Mehul",
-            "Contact" : "2501730149",
-            success: true
-        }))
+        res.end('<h1>Contact Page</h1>')
     }
     else {
         res.statusCode = 404

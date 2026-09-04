d173fd1baf32166f4f048b4f707806b6debb7c2c
Mehul Srivastava <srimehul18@gmail.com>
2026-09-04 22:31:10 +0530

    Added serevr.js to create HTTP local server

---
+++ server.js
@@ -0,0 +1,26 @@
+import http from 'http'
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/' && req.method === 'GET') {
+        res.end('<h1>Welcome to Assignment 1</h1>')
+    }
+    else if (req.url === '/about' && req.method === 'GET') {
+        res.end('<h1>About Page</h1> <p>All about infro can be found here</p>')
+    }
+    else if (req.url === '/contact' && req.method === 'GET') {
+        res.end(JSON.stringify({
+            "Name": "Mehul",
+            "Contact" : "2501730149",
+            success: true
+        }))
+    }
+    else {
+        res.statusCode = 404
+        res.end('Error: 404 - Page Not Found')
+    }
+})
+const port = 3000
+
+server.listen(port, () => {
+    console.log('Server has started on port', port)
+})
